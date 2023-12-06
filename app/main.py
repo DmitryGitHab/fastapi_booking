cf962a7ebb389f6283ac2d49af0fa119ca274bce
Dmitry <babanov91@gmail.com>
2023-12-06 22:31:34 +0500

    add typing

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
-@app.get("/hotels")
-def get_hotels():
-    return "Hotel Resort 5*"
+@app.get("/hotels/")
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+):
+    return f"{location}, {date_from}, {date_to}, {has_spa}, {stars}"
+
+
+# import requests
+#
+# r = requests.get(
+#     "http://127.0.0.1:8000/hotels/5",
+#     params={'date_from':'today', 'date_to':'tomorrow'}
+# )
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+```.venv\Scripts\activate.bat```
+
+```pip install -r requirements.txt```
+
+uvicorn app.main:app --reload --port 8080    
