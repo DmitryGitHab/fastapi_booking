8da85915bfd61946a685118b6ff7b8e4fc458b90
Dmitry <babanov91@gmail.com>
2023-12-12 22:23:36 +0500

    fix get bookings join

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -2,21 +2,30 @@ from fastapi import APIRouter
 from sqlalchemy import select
 
 from app.bookings.models import Bookings
+from app.hotels.rooms.models import Rooms
 from app.database import async_session_maker
 
 router = APIRouter(
     prefix='/bookings',
     tags=['Бронирование']
 )
-
+#
 @router.get('')
 async def get_bookings():
     async with async_session_maker() as session:
         quere = select(Bookings.__table__.columns)
         result = await session.execute(quere)
-        return result.mappings().all()
+        return result.mappings().all(),
 
+@router.get('/{user_id}')
+async def get_bookings_join(user_id:int):
+    print(user_id)
+    async with async_session_maker() as session:
+        quere = select(Bookings.__table__.columns, Rooms.__table__.columns,).join(Rooms, Rooms.id == Bookings.room_id, isouter=True).where(Bookings.user_id == user_id)
+        result = await session.execute(quere)
+        return result.mappings().all()
+        # return user_id
 
 @router.post('/{booking_id}')
-def get_bookings(booking_id):
+def get_bookings(booking_id:int):
     return booking_id
